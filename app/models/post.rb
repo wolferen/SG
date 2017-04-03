31452a2e55b785f278d7206f4dc5a3594244050f
wolferen <veliar.master@gmail.com>
2017-04-03 23:10:14 +0300

    index

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
   attr_accessible :body, :published_at, :title
   has_many :images, :as => :imageable
   belongs_to :user
+
+  scope :published_all, -> { where('published_at IS NOT NULL') }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,16 @@ class User < ActiveRecord::Base
 
   validates :email, :uniqueness => true, :presence => true
   validates :username, :uniqueness => true, :presence => true
-  validates :password, :lenght => { :minimum => 8 }, :presence => true
+  validates :password, :length => { :minimum => 8 }, :presence => true
 
-  scope :age, -> {where ()}
+  # scope :age, -> {where (:bithday > Date.today - 18.years)}
+
+  # def full_name
+  #   "#{first_name} #{last_name}"
+  # end
+
+  scope :active_user, -> { where email.present? }
+  scope :birthdate, -> { active_user.where('birthdate <=?', Time.now - 18.year) }
+
+
 end
